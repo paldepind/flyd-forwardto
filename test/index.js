65edd8637695779af67a869361c894769364ecad
paldepind <simonfv@gmail.com>
2015-05-29 19:55:09 +0200

    Let no-one have a reference to the created stream

--- test/index.js
+++ test/index.js
@@ -13,15 +13,4 @@ describe('forwardTo', function() {
     fw(1)(2)(3);
     assert.deepEqual(result, [2, 1, 4, 2, 6, 3]);
   });
-  it('ends when target ends', function() {
-    var result = [];
-    var target = flyd.stream();
-    function fn(v) { result.push(v); }
-    var fw = forwardTo(target, fn);
-    fw(1)(2)(3);
-    assert.deepEqual(result, [1, 2, 3]);
-    assert.notEqual(fw.end(), true);
-    target.end(true);
-    assert.strictEqual(fw.end(), true);
-  });
 });
